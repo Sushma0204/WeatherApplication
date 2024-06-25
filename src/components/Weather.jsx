dfd16eee19127a875a346f4d2fff353542d1fcbd
Sushma <98072240+Sushma0204@users.noreply.github.com>
2024-06-25 18:42:35 +0530

    Update Weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -52,7 +52,7 @@ const Weather = () => {
       return;
     }
     try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_API}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=fd5671ae72d18ee0509955d32c83f217`
       const response = await fetch(url);
       const data = await response.json();
       if (!response.ok) {
